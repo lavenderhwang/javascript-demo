e7dd5b0755cd5706ae4ece827adbf1f4dce959ab
Lavender Hwang <lhwang914@gmail.com>
2025-10-16 10:40:39 -0700

    Set up demo

---
+++ script.js
@@ -0,0 +1,28 @@
+// Step 1: grab elements from DOM
+
+// const input = document.getElementById("taskInput");
+// const addBtn = document.getElementById("addBtn");
+// const list = document.getElementById("taskList");
+
+// Step 2: activtate event listener
+//addBtn.addEventListener("click", function () {
+//   alert("Button is clicked!");
+// });
+
+// Step 3: get input + add task
+addBtn.addEventListener("click", function () {
+  //   const taskText = input.value;
+  // 1) Create a new <li> element
+  //   const newTask = document.createElement("li");
+  //   newTask.innerText = taskText;
+  // 2) Add it to the <ul>
+  //   list.appendChild(newTask);
+  // 3) Clear the input field
+  //   input.value = "";
+  // Challenge: Step 4: add a remove button
+  //   const removeBtn = document.createElement("button");
+  //   removeBtn.innerText = "x";
+  //   removeBtn.classList.add("remove");
+  //   newTask.appendChild(removeBtn);
+  //   removeBtn.addEventListener("click", () => newTask.remove());
+});
